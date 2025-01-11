7c048dd06b4324b6146c22c24959e16e3c2e30eb
SheshnathRajbhar <rajbharsheshnath072@gmail.com>
2025-01-11 10:31:57 +0530

    e

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div class="main container" id="hero">
     <div class="main_left">
         <div class="main_left_content">
-            <h1 class="main_left_content_title">Hello, I'm <span>Sheshnath</span></h1>
+            <h1 class="main_left_content_title">Hi, I'm <span>Sheshnath Rajbhar</span></h1>
             <p class="main_left_content_description">Sheshnath is a skilled developer specializing in both frontend and backend development. With expertise in HTML, CSS, JavaScript, and backend technologies like node.js and Python, he builds dynamic, scalable web solutions. Additionally, Sheshnath leverages Google Analytics to optimize user experience and measure website performance. His technical proficiency ensures seamless, data-driven, and responsive web applications.</p>
             <button class="main_left_content_button"><a href="CVV.docx" download="">Download CV</a></button>
         </div>
